b7b03b1bdda2395aa621f67eb1cc0635edf110d0
Enirose <enirosehellum@gmail.com>
2022-09-15 04:29:28 +0200

    Fetching Blog List API (undone)

--- js/blog.js
+++ js/blog.js
@@ -1,48 +1,30 @@
-const url = `https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed`;
+const url = "https://blog-api.enirosehellum.com/wp-json/wp/v2/posts";
 const blogPost = document.querySelector(".blog-post");
 
-// fetching Blog Api
+async function fetchApi() {
 
-async function fetchApi(url) {
     try {
         const response = await fetch(url);
-        const details = await response.json();
-        console.log(details);
-        createBlog(details);
+        const result = await response.json();
+        console.log(result);
+
+        blogPost.innerHTML = "";
+
+        for (let i = 0; i < result.length; i++) {
+            blogPost.innerHTML += `<div class="blog-post">
+                <div class="blog">
+                    <a href="post.html?id=${result[i].id}"><img src="${result[i]._links["wp:featuredmedia"][0].href}" alt="Blog"</img>
+                    <h2>${result[i].title.rendered}</h2>
+                        <p>${result[i].excerpt.rendered}</p>
+                    </a>
+                </div>
+            </div>`;
+        }
     }
     catch (error) {
-        console.log(error);
-    };
+        console.log("An error occured");
+        blogPost.innerHTML = errorMessage("An error occured while fetching the results");
+    }
 }
 
 fetchApi();
-function createBlog(details) {
-    blogPost.innerHTML = `<div class="blog">
-                    <a href="post.html"><img src="${json[i].images[0].thumbnail}" alt="Blog Picture">
-                        <h2>${json.title.rendered}</h2>
-                        <p>${json.excerpt.rendered}</p>
-                    </a>
-                </div>`;
-}
-
-
-// async function fetchApi(url) {
-//     const res = await fetch(url);
-//     const blogs = await res.json();
-
-//     createBlog
-
-//     blogPost.innerHTML = "";
-
-//     for (let i = 0; i < json.lenght; i++) {
-//         blogPost.innerHTML += `<div class="blog">
-//                     <a href="post.html"><img src="${json[i].images[0].thumbnail}" alt="Blog Picture">
-//                         <h2>${json.title.rendered}</h2>
-//                         <p>${json.excerpt.rendered}</p>
-//                     </a>
-//                 </div>`;
-//     };
-
-// }
-
-// fetchApi();
--- js/component/errormessage.js
+++ js/component/errormessage.js
@@ -2,5 +2,5 @@ function errorMessage(message, err) {
     if (err === undefined) {
         err = "error"
     }
-    return `<div class ="${err}">${message}</div>`;
+    return `<div class ="error">${message}</div>`;
 }
