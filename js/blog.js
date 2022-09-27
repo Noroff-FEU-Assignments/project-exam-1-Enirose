881c0273c13bd48a4938e35c8d5e07e39c1d8105
Enirose <enirosehellum@gmail.com>
2022-09-27 13:41:27 +0200

    fetch images

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
-const url = "https://blog-api.enirosehellum.com/wp-json/wp/v2/posts";
+const url = "https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed";
 const blogPost = document.querySelector(".blog-post");
 
 async function fetchApi() {
@@ -13,9 +13,9 @@ async function fetchApi() {
         for (let i = 0; i < result.length; i++) {
             blogPost.innerHTML += `<div class="blog-post">
                 <div class="blog">
-                    <a href="post.html?id=${result[i].id}"><img src="${result[i]._links["wp:featuredmedia"][0].href}" alt="Blog"</img>
+                    <a href="post.html?id=${result[i].id}">
+                    <img src="${result[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Blog"</img>
                     <h2>${result[i].title.rendered}</h2>
-                        <p>${result[i].excerpt.rendered}</p>
                     </a>
                 </div>
             </div>`;
