ad58f66cda85a62407abaecd318107a14d7022a5
Enirose <enirosehellum@gmail.com>
2022-10-05 11:36:37 +0200

    working on view more button

--- js/blog.js
+++ js/blog.js
@@ -1,27 +1,135 @@
-const url = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed';
+// const url = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed';
+// // let loadMore = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=20&_embed';
+// const blogPost = document.querySelector(".blog-post");
+// const viewBtn = document.querySelector(".cta")
+// // const addMorePosts = "&per_page=20"
+
+// async function fetchApi() {
+
+//     try {
+//         const response = await fetch(url);
+//         const result = await response.json();
+//         // console.log(result);
+
+//         blogPost.innerHTML = "";
+
+//         for (let i = 0; i < result.length; i++) {
+//             blogPost.innerHTML += `<div class="blog-post">
+//                 <div class="blog">
+//                     <a href="post.html?id=${result[i].id}">
+//                     <img src="${result[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Blog">
+//                     <h2>${result[i].title.rendered}</h2>
+//                     </a>
+//                 </div>
+//             </div>`;
+//         }
+//     }
+//     catch (error) {
+//         console.log("An error occured");
+//         blogPost.innerHTML = errorMessage("An error occured while fetching the results");
+//     }
+// }
+
+
+// fetchApi();
+
+// // ////////// adding view more posts///////////
+
+// viewBtn.onclick = function () {
+//     blogPost.innerHTML = "";
+//     let loadMore = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=20&_embed';
+//     console.log(loadMore);
+//     fetchApi(loadMore);
+//     viewBtn.style.display = "none";
+
+// };
+
+// const fetchPostsUrl = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed';
+// const loadMore = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=20&_embed';
+// const blogPost = document.querySelector(".blog-post");
+// const viewBtn = document.querySelector(".cta")
+// // const addMorePosts = "&per_page=20"
+
+// async function fetchApi(isLoadMore) {
+//     let url = fetchPostsUrl
+//     if (isLoadMore === true) {
+//         url = loadMore
+//     }
+//     try {
+//         const response = await fetch(url);
+//         const result = await response.json();
+//         // console.log(result);
+
+//         if (isLoadMore === false) {
+//             blogPost.innerHTML = "";
+//         }
+
+
+//         for (let i = 0; i < result.length; i++) {
+//             blogPost.innerHTML += `<div class="blog-post">
+//                 <div class="blog">
+//                     <a href="post.html?id=${result[i].id}">
+//                     <img src="${result[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Blog">
+//                     <h2>${result[i].title.rendered}</h2>
+//                     </a>
+//                 </div>
+//             </div>`;
+//         }
+//     }
+//     catch (error) {
+//         console.log("An error occured");
+//         blogPost.innerHTML = errorMessage("An error occured while fetching the results");
+//     }
+// }
+
+
+// fetchApi(false);
+
+// // ////////// adding view more posts///////////
+
+// viewBtn.onclick = function () {
+//     blogPost.innerHTML = "";
+//     console.log(loadMore);
+//     fetchApi(true);
+//     viewBtn.style.display = "none";
+
+// };
+
+// const url = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed';
 // let loadMore = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=20&_embed';
+```js
+let fetchPostsUrl = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=${numberOfPagesToGet}&offset=${pageNumberToGet}&_embed';
 const blogPost = document.querySelector(".blog-post");
 const viewBtn = document.querySelector(".cta")
+let numberOfPagesToGet = 5;
 // const addMorePosts = "&per_page=20"
 
-async function fetchApi() {
+async function fetchApi(isLoadMore) {
+    numberOfPagesToGet = numberOfPagesToGet + 1;
+    let url = fetchPostsUrl;
 
+    if (isLoadMore === true) {
+        url = fetchPostsUrl
+    }
     try {
-        const response = await fetch(url);
+        const response = await fetch(fetchPostsUrl);
         const result = await response.json();
-        // console.log(result);
+        console.log(result);
 
-        blogPost.innerHTML = "";
+        if (isLoadMore === false) {
+            blogPost.innerHTML = "";
+        }
+
 
         for (let i = 0; i < result.length; i++) {
-            blogPost.innerHTML += `<div class="blog-post">
-                <div class="blog">
-                    <a href="post.html?id=${result[i].id}">
-                    <img src="${result[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Blog">
-                    <h2>${result[i].title.rendered}</h2>
-                    </a>
-                </div>
-            </div>`;
+            blogPost.innerHTML += `< div class="blog-post" >
+    <div class="blog">
+        <a href="post.html?id=${result[i].id}">
+            <img src="${result[i]._embedded[" wp:featuredmedia"][0].source_url}" alt="Blog">
+            <h2>${result[i].title.rendered}</h2>
+        </a>
+    </div>
+            </div > `;
         }
     }
     catch (error) {
@@ -31,16 +139,15 @@ async function fetchApi() {
 }
 
 
-fetchApi();
+fetchApi(false);
 
 // ////////// adding view more posts///////////
 
 viewBtn.onclick = function () {
     blogPost.innerHTML = "";
-    let loadMore = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=20&_embed';
-    console.log(loadMore);
-    fetchApi(loadMore);
+    fetchApi();
     viewBtn.style.display = "none";
 
 };
 
+```
