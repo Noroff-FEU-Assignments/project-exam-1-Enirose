20f63a13dbc3b225a4e789ae43460bd049424247
Enirose <enirosehellum@gmail.com>
2022-10-05 13:03:03 +0200

    almost working view more button

--- js/blog.js
+++ js/blog.js
@@ -97,57 +97,50 @@
 
 // const url = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?_embed';
 // let loadMore = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=20&_embed';
-```js
-let fetchPostsUrl = 'https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=${numberOfPagesToGet}&offset=${pageNumberToGet}&_embed';
-const blogPost = document.querySelector(".blog-post");
-const viewBtn = document.querySelector(".cta")
-let numberOfPagesToGet = 5;
-// const addMorePosts = "&per_page=20"
+
+let numberOfPagesToGet = 10;
+let pageNumberToGet = 0;
+const fetchPostsUrl = `https://blog-api.enirosehellum.com/wp-json/wp/v2/posts?per_page=${numberOfPagesToGet}&offset=${pageNumberToGet}&_embed`;
+const blogPost = document.querySelector('.blog-post');
+const viewBtn = document.querySelector('.cta');
 
 async function fetchApi(isLoadMore) {
-    numberOfPagesToGet = numberOfPagesToGet + 1;
-    let url = fetchPostsUrl;
+    //   numberOfPagesToGet = numberOfPagesToGet + 1;
 
-    if (isLoadMore === true) {
-        url = fetchPostsUrl
-    }
     try {
         const response = await fetch(fetchPostsUrl);
         const result = await response.json();
         console.log(result);
 
         if (isLoadMore === false) {
-            blogPost.innerHTML = "";
+            blogPost.innerHTML = '';
         }
-
 
         for (let i = 0; i < result.length; i++) {
-            blogPost.innerHTML += `< div class="blog-post" >
-    <div class="blog">
-        <a href="post.html?id=${result[i].id}">
-            <img src="${result[i]._embedded[" wp:featuredmedia"][0].source_url}" alt="Blog">
-            <h2>${result[i].title.rendered}</h2>
-        </a>
-    </div>
-            </div > `;
+            console.log(result[i]);
+            blogPost.innerHTML += `
+      <div class="blog-post" >
+        <div class="blog">
+            <a href="post.html?id=${result[i].id}">
+                <img src="${result[i]._embedded['wp:featuredmedia'][0].source_url}" alt="Blog">
+                <h2>${result[i].title.rendered}</h2>
+            </a>
+        </div>
+    </div > `;
         }
-    }
-    catch (error) {
-        console.log("An error occured");
-        blogPost.innerHTML = errorMessage("An error occured while fetching the results");
+    } catch (error) {
+        console.log(error);
+        console.log('An error occured');
+        blogPost.innerHTML = errorMessage('An error occured while fetching the results');
     }
 }
-
 
 fetchApi(false);
 
 // ////////// adding view more posts///////////
 
 viewBtn.onclick = function () {
-    blogPost.innerHTML = "";
-    fetchApi();
-    viewBtn.style.display = "none";
-
+    pageNumberToGet++;
+    fetchApi(true);
+    viewBtn.style.display = 'none';
 };
-
-```
